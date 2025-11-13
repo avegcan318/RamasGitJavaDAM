a104f3e04407e4c2d4ab2c1f157852bc765c03c8
Antonio Vega <avegcan318@g.educaand.es>
2025-11-13 10:27:53 +0100

    Estructura Eclipse + Main con Lotería (inicial)

--- src/antonio/vega/ssh/Main.java
+++ src/antonio/vega/ssh/Main.java
@@ -1,28 +1,20 @@
 package antonio.vega.ssh;
 
 import java.util.Random;
+import java.util.HashSet;
+
 
 public class Main {
 	public static void main(String[] args) {
-		// Investiga el uso de random y genera por pantalla los números de la lotería
-		// primitiva.
-		Random random = new Random();
-		int num1 = 0, num2 = 0, num3 = 0, num4 = 0, num5 = 0, comp = 0;
-
-		// Genera 5 números principales
-		num1 = random.nextInt(9);
-		num2 = random.nextInt(9);
-		num3 = random.nextInt(9);
-		num4 = random.nextInt(9);
-		num5 = random.nextInt(9);
-		// Número complementario
-		comp = random.nextInt(9);
-		System.out.printf("Números de la Lotería Primitiva: %d%d%d%d%d", num1, num2, num3, num4, num5);
-
-		System.out.printf("\nNúmero Complemetario: %d", comp);
-
-		System.out.println("¡Buena suerte en el sorteo!");
-		System.out.println("Cambio realizado desde GitHub.");
+		// Lotería Primitiva: 5 números principales (1..49) + 1 complementario
+		HashSet<Integer> bolas = new HashSet<>();
+		Random r = new Random();
+		while (bolas.size() < 5) {
+			bolas.add(r.nextInt(49) + 1);
+		}
+		System.out.println("Números principales: " + bolas);
+		int complementario = r.nextInt(49) + 1;
+		System.out.println("Complementario: " + complementario);
 
 	}
 }
